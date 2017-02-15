959bb5a0caddc810033f58e6ce4229e7e5cc9f3c
leskal <alexakis1997@gmail.com>
2017-02-15 17:45:42 +0200

    Files from 12/1 OTA
    * Remove some useless files and fix youtube prob

--- product/audio.mk
+++ product/audio.mk
@@ -12,6 +12,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/audio_device.xml:system/etc/audio_device.xml \
-    $(LOCAL_PATH)/audio/audio_em.xml:system/etc/audio_em.xml
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_em.xml:system/etc/audio_em.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf
