a6f20b9bc42ca55287120418295e45f0b8f6a5f4
tribual <tribual@web.de>
2017-03-15 11:18:20 +0100

    aokp-ify

--- board/kernel.mk
+++ board/kernel.mk
@@ -7,7 +7,7 @@ BOARD_MKBOOTIMG_ARGS := --board 1465391499 --ramdisk_offset 0x04f88000 --second_
 
 # Kernel properties
 TARGET_KERNEL_SOURCE := kernel/elephone/p9000
-TARGET_KERNEL_CONFIG := lineage_p9000_defconfig
+TARGET_KERNEL_CONFIG := aokp_p9000_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_ARCH := arm64		
 TARGET_KERNEL_HEADER_ARCH := arm64
@@ -17,7 +17,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 # Kernel informations
 # BOARD_KERNEL_BASE := 0x40078000
 # BOARD_KERNEL_PAGESIZE := 2048
-# BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+# BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 # BOARD_MKBOOTIMG_ARGS := --board 1465391499 --ramdisk_offset 0x04f88000 --second_offset 0x00e88000 --tags_offset 0x03f88000
 
 # Kernel properties
