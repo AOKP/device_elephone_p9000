8616612b2eb439164df60ac23c5f53866651f915
leskal <alexakis1997@gmail.com>
2017-05-16 14:25:50 +0300

    Patches for N

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="frameworks/av frameworks/native hardware/libhardware system/core system/sepolicy"
+dirs="bionic frameworks/av frameworks/base frameworks/native hardware/libhardware system/core system/sepolicy"
 
 for dir in $dirs ; do
 	cd $rootdirectory
