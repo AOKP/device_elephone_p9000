f92725096ebf27b85c2c604a0e3dbfa711a5ddc8
leskal <alexakis1997@gmail.com>
2017-05-19 14:31:29 +0300

    Move some symbols from patches to libmtk_symbols
    * Remove useless patches
    * Add missing symbols
    * Add missing script for charging
    * Add missing partitions in ueventd
    
    Change-Id: I3e13220708adc75bac43ac605ce761aca53b0b95

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/av frameworks/base frameworks/native hardware/libhardware system/core"
+dirs="bionic frameworks/av frameworks/base system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory
