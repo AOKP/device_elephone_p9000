76124f1c862345b0c389b968ab61ffb628b1ae6a
tribual <tribual@web.de>
2017-12-01 19:56:26 +0100

    little more changes

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="frameworks/av frameworks/base system/netd"
+dirs="frameworks/av frameworks/base system/netd hardware/libhardware"
 
 for dir in $dirs ; do
 	cd $rootdirectory
